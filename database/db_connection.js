14cd508b69150199e6554a000ab91114533f38e1
Federico Barrientos <fgbarrientos93@gmail.com>
2020-06-19 15:36:05 -0300

    DB UPDATE

---
+++ database/db_connection.js
@@ -0,0 +1,13 @@
+const db_user = "root";
+const db_password = "";
+const db_host = "localhost";
+const db_port = "3306";
+const db_name = "delilah_resto";
+
+module.exports = {
+    db_user,
+    db_password,
+    db_host,
+    db_port,
+    db_name
+};
