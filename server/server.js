4c6948076f24356a377d41e84e9cae984f3b4f14
Federico Barrientos <fgbarrientos93@gmail.com>
2020-07-09 13:20:31 -0300

    8/7
    Update not working

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 //Express
 const express = require('express');
 const server = express ();
+
 //Body Parser
 const bodyParser = require('body-parser');
+
 //JSON Web Token
 const jwt = require('jsonwebtoken');
 const JWTSing = "Delilah_2020";
+
 // DB Setup
 const Sequelize = require('sequelize');
 const { db_host, db_name, db_user, db_password, db_port } = require("../database/db_connection");
 const db = new Sequelize(`mysql://${db_user}:${db_password}@${db_host}:${db_port}/${db_name}`);
-
 
 
 //Server
@@ -19,6 +21,119 @@ server.listen(3000, () => {
     console.log("Server initialized " + date);
 })
 server.use(bodyParser.json());
+
+
+////ROUTES PRODUCTS/////
+//get all products
+server.get('/products',(req, res) => {
+    db.query(
+        'SELECT * FROM products WHERE is_active = TRUE',{
+            type: db.QueryTypes.SELECT
+        })
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({
+                mensaje: 'Ocurrió un error con la base de datos',
+                err: err
+        });
+    });
+});
+
+//get one product by id
+server.get('/products/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query(
+        `SELECT * FROM products WHERE product_id = :id`, {
+            type: db.QueryTypes.SELECT,
+            
+                replacements: {id: id}
+        })
+        .then(response => {
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({
+                mensaje: 'Ocurrió un error con la base de datos',
+                err: err
+        });
+    });
+})
+
+
+//post a new product
+server.post('/products', validateInfo, (req, res) => {
+    const newProduct = req.body;
+    db.query(
+        `INSERT INTO products(
+            name,
+            price,
+            description,
+            img_url
+        )
+        VALUE(
+            :name,
+            :price,
+            :description,
+            :img_url
+        )`,{
+            replacements: newProduct
+        })
+        .then(() => {
+            res.status(200).json("The product (" + newProduct.name + ") has been added to the menu");
+        })
+        .catch(err =>{
+            res.json({
+                mensaje:"Ocurrio un error inesperado",
+                err: err
+        });
+    })
+})
+
+//update a existing user (todavia no funciona)
+
+// server.put('products/:id', (req, res) => {
+//     const id = req.params.id;
+//     const newValues = req.body;
+//     console.log("HOLIS" + id);
+//     console.log("GG" + newValues);
+
+//     db.query(
+//         `SELECT * FROM products WHERE product_id = :id`, {
+//             type: db.QueryTypes.SELECT,
+            
+//                 replacements: {id: id}
+//         })
+//         .then(response => {
+//             if(response.length !== 0) {
+//                 db.query(`UPDATE products
+//                  SET name = :name, price = :price, description = :description, img_url = :img_url, is_active = :is_active
+//                  WHERE product_id = :id`,{
+//                     replacements: {
+//                         name: newValues.name,
+//                         price: newValues.price,
+//                         description: newValues.description,
+//                         img_url: newValues.img_url,
+//                         is_active: newValues.is_active,
+//                         id: id
+//                     }
+//                  })
+//             }else {
+//                 res.status(404).json({
+//                     mensaje: 'product not found'
+//                 })
+//             }
+//         })
+//         .catch(err =>{
+//             res.json({
+//                 mensaje:"Ocurrio un error inesperado",
+//                 err: err
+//         });
+//     })
+// })
+
 
 /////ROUTES USERS/////
 //get all users
@@ -71,73 +186,6 @@ server.post('/users', (req,res) => {
             err: err
         });
     });
-})
-
-////ROUTES PRODUCTS/////
-//get all products
-server.get('/products',(req, res) => {
-    db.query(
-        'SELECT * FROM products WHERE is_active = TRUE',{
-            type: db.QueryTypes.SELECT
-        })
-        .then(response => {
-            res.status(200).json(response);
-        })
-        .catch(err => {
-            res.status(500).json({
-                mensaje: 'Ocurrió un error con la base de datos',
-                err: err
-        });
-    });
-});
-
-//get one product by id
-server.get('/products/:id', (req, res) => {
-    productID = req.params.id;
-    db.query(
-        `SELECT * FROM products WHERE id = :id`, {
-            replacements: productID,
-            type: db.QueryTypes.SELECT
-        })
-        .then(response => {
-            res.status(200).json(response);
-        })
-        .catch(err => {
-            res.status(500).json({
-                mensaje: 'Ocurrió un error con la base de datos',
-                err: err
-        });
-    });
-})
-
-
-//post a new product
-server.post('/products', validateInfo, (req, res) => {
-    const newProduct = req.body;
-    db.query(
-        `INSERT INTO products(
-            name,
-            price,
-            description,
-            img_url
-        )
-        VALUE(
-            :name,
-            :price,
-            :description,
-            :img_url
-        )`,{
-            replacements: newProduct
-        })
-        .then(() => {
-            res.status(200).json("The product (" + newProduct.name + ") has been added to the menu");
-        })
-        .catch(err =>{
-            res.json({
-                mensaje:"Ocurrio un error inesperado",
-                err: err
-        });
-    })
 })
 
 //middlewares//////////////////////////////////
